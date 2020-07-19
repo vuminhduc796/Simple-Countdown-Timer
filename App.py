caac452b88c57a31d69bd0594229d0f3b4b50cab
Duc <mvuu0003@student.monash.edu>
2020-07-19 17:40:32 +1000

    Changed in UI, fixing clock issue

--- App.py
+++ App.py
@@ -3,63 +3,79 @@ import time
 from tkinter import *
 
 from threading import Thread
-import os
 
 class PopUp(object):
     def __init__(self,master):
         self.task = None
         top = self.top = Toplevel(master)
-        self.l = Label(top, text="Enter Task", padx=60, pady=20,fg = "#074a35", bg="#79e8c6",font=("Comic Sans MS",15)).grid(row=0,column=0,columnspan=6)
-        self.l1 = Label(top, text="Task Name", padx=40, pady=20, font=(None, 10)).grid(row=1, column=0)
-        self.l1 = Label(top, text="Duration", font=(None, 10)).grid(row=1, column=3,columnspan=2)
-        self.entry_name = Entry(top)
+        self.l = Label(top, text="Enter Task", padx=64, pady=20,fg = "#381310", bg="#de9e99",font=("Comic Sans MS",25,"bold")).grid(row=0,column=0,columnspan=6)
+        self.l1 = Label(top, text="Task Name", padx=30, pady=10,fg = "#473433", bg="#bfadac", font=("Comic Sans MS", 15)).grid(row=1, column=0,columnspan=3)
+        self.l2 = Label(top, text="Duration",padx=30, pady=10,fg = "#473433", bg="#bfadac", font=("Comic Sans MS", 15)).grid(row=1, column=3,columnspan=3)
+        self.entry_name = Entry(top,font=("Calibri 12"))
         self.entry_name.grid(row=3,column=0,columnspan=3)
-
-        self.entry_hour = Entry(top,width = 2)
+        self.entry_hour = Entry(top,width = 2,font=("Calibri 12"))
         self.entry_hour.insert(END, "00")
-        self.entry_min = Entry(top,width = 2)
+        self.entry_min = Entry(top,width = 2,font=("Calibri 12"))
         self.entry_min.insert(END, "00")
-        self.entry_sec = Entry(top,width = 2)
+        self.entry_sec = Entry(top,width = 2,font=("Calibri 12"))
         self.entry_sec.insert(END, "00")
 
         self.photo_down = PhotoImage(file="down.png")
         self.photo_up = PhotoImage(file = "up.png")
-        self.up1 = Button(top, image = self.photo_up,height = 10, width = 10,command=lambda: self.increase("hour")).grid(row=2, column=3)
-        self.up2 = Button(top, image= self.photo_up,height = 10, width = 10,command=lambda: self.increase("min")).grid(row=2, column=4)
-        self.up3 = Button(top, image= self.photo_up,height = 10, width = 10,command=lambda: self.increase("sec")).grid(row=2, column=5)
+        self.up1 = Button(top, image = self.photo_up,height = 10, width = 10,command=lambda: self.increase("hour")).grid(row=2, column=3,pady =5)
+        self.up2 = Button(top, image= self.photo_up,height = 10, width = 10,command=lambda: self.increase("min")).grid(row=2, column=4,pady =5)
+        self.up3 = Button(top, image= self.photo_up,height = 10, width = 10,command=lambda: self.increase("sec")).grid(row=2, column=5,pady =5)
         self.entry_hour.grid(row=3,column=3,padx=5)
         self.entry_min.grid(row=3, column=4,padx=5)
         self.entry_sec.grid(row=3, column=5,padx=5)
-        self.down1 = Button(top, image=self.photo_down,height = 10, width = 10,command=lambda: self.decrease("hour")).grid(row=4, column=3)
-        self.down2 = Button(top, image=self.photo_down,height = 10, width = 10,command=lambda: self.decrease("min")).grid(row=4, column=4)
-        self.down3 = Button(top, image=self.photo_down,height = 10, width = 10,command=lambda: self.decrease("sec")).grid(row=4, column=5)
+        self.down1 = Button(top, image=self.photo_down,height = 10, width = 10,command=lambda: self.decrease("hour")).grid(row=4, column=3,pady =5)
+        self.down2 = Button(top, image=self.photo_down,height = 10, width = 10,command=lambda: self.decrease("min")).grid(row=4, column=4,pady =5)
+        self.down3 = Button(top, image=self.photo_down,height = 10, width = 10,command=lambda: self.decrease("sec")).grid(row=4, column=5,pady =5)
 
-        self.b = Button(top, text='Save', padx=20, pady=5,command=self.cleanup).grid(row=5,column=0,columnspan=6)
+        self.b = Button(top, text='Save', padx=20, pady=5,bg="#bfadac",command=self.cleanup).grid(row=6,column=0,columnspan=6, pady =5)
 
     def increase(self,type):
         if type == "hour":
-            new_val = str(int(self.entry_hour.get()) + 1)
+            if int(self.entry_hour.get()) >= 23:
+                new_val = "00"
+            else:
+                new_val = str(int(self.entry_hour.get()) + 1)
             self.entry_hour.delete(0, END)
             self.entry_hour.insert(0, new_val)
         elif type == "min":
-            new_val = str(int(self.entry_min.get()) + 1)
+            if int(self.entry_min.get()) >= 59:
+                new_val = "00"
+            else:
+                new_val = str(int(self.entry_min.get()) + 1)
             self.entry_min.delete(0, END)
             self.entry_min.insert(0, new_val)
         elif type == "sec":
-            new_val = str(int(self.entry_sec.get()) + 1)
+            if int(self.entry_sec.get()) >= 59:
+                new_val = "00"
+            else:
+                new_val = str(int(self.entry_sec.get()) + 1)
             self.entry_sec.delete(0, END)
             self.entry_sec.insert(0, new_val)
     def decrease(self,type):
         if type == "hour":
-            new_val = str(int(self.entry_hour.get()) - 1)
+            if int(self.entry_hour.get()) <= 0:
+                new_val = "59"
+            else:
+                new_val = str(int(self.entry_hour.get()) - 1)
             self.entry_hour.delete(0, END)
             self.entry_hour.insert(0, new_val)
         elif type == "min":
-            new_val = str(int(self.entry_min.get()) - 1)
+            if int(self.entry_min.get()) <= 0:
+                new_val = "59"
+            else:
+                new_val = str(int(self.entry_min.get()) - 1)
             self.entry_min.delete(0, END)
             self.entry_min.insert(0, new_val)
         elif type == "sec":
-            new_val = str(int(self.entry_sec.get()) - 1)
+            if int(self.entry_sec.get()) <= 0:
+                new_val = "59"
+            else:
+                new_val = str(int(self.entry_sec.get()) - 1)
             self.entry_sec.delete(0, END)
             self.entry_sec.insert(0, new_val)
     def cleanup(self):
@@ -85,11 +101,11 @@ class CountDown:
         thread.start()                                  # Start the execution
     def add_task(self,task):
         self.task_list.append(task)
-        task_name = Label(root, text=task.name)
-        task_name.grid(row=len(self.task_list), column=0)
+        task_name = Label(root, text=task.name,fg = "#381310", bg="#de9e99",font=("Comic Sans MS",15))
+        task_name.grid(row=len(self.task_list), column=0, pady = 5,padx = 10)
         task_name.bind("<Button-1>", lambda e: self.clock_trigger(self.task_list.index(task)))
-        duration = Label(root, text=task.duration[0]+" : "+task.duration[1]+" : "+task.duration[2])
-        duration.grid(row=len(self.task_list), column=1)
+        duration = Label(root, text=task.duration[0]+" : "+task.duration[1]+" : "+task.duration[2],fg = "#381310", bg="#de9e99",font=("Comic Sans MS",15))
+        duration.grid(row=len(self.task_list), column=1, pady = 5)
 
     def edit(self):
     
@@ -118,7 +134,7 @@ class CountDown:
             new_duration = [current[0],current[1],int(current[2]) - 1]
         self.task_list[index].duration = new_duration
 
-        duration = Label(root, text=str(new_duration[0])+" : "+str(new_duration[1])+" : "+str(new_duration[2]))
+        duration = Label(root, text=str(new_duration[0])+" : "+str(new_duration[1])+" : "+str(new_duration[2]),fg = "#381310", bg="#de9e99",font=("Comic Sans MS",15))
         duration.grid(row=index + 1, column=1)
 
 
@@ -139,7 +155,7 @@ class main(object):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Effective Countdown App")
+    root.title("Countdown App")
     m=main(root)
     root.mainloop()
 
